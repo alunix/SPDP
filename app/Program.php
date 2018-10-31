577fe41a12f3d06ea482ceeb68b67753e65671e9
bezane97 <syazany1997@gmail.com>
2018-10-31 22:32:01 +0800

    Added attribute doc_title to the program model

--- app/Program.php
+++ app/Program.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Program extends Model
 {
     protected $fillable = [
-       'lecturer_name', 'fakulti' , 'file_link' ,'status_program','file_name'
+       'lecturer_name', 'fakulti' , 'file_link' ,'status_program','file_name','doc_title'
 
 
    ];

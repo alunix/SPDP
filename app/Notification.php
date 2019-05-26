4a7be2dd02d6406362f2552b901acf3a527644f1
bezane97 <syazany1997@gmail.com>
2019-05-26 18:06:22 +0800

    Relationship Notificication

--- app/Notification.php
+++ app/Notification.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    
+
+
+public function status(){
+    return $this->belongsTo('SPDP\StatusPermohonan','notificationDetails');
+}
+
+
+
 }

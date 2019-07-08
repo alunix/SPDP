493c4d107b5e056045566f649bcff778c2f4e5b3
bezane97 <syazany1997@gmail.com>
2019-07-08 22:32:11 +0800

    Deleted database erro

--- config/database.php
+++ config/database.php
@@ -1,13 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-define('RDS_HOSTNAME',$_SERVER['RDS_HOSTNAME']);
-define('RDS_USERNAME',$_SERVER['RDS_USERNAME']);
-define('RDS_PASSWORD',$_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME',$_SERVER['RDS_DB_NAME']);
-
-=======
->>>>>>> parent of 5e49f77... Get database password/username/ in database.php for beanstalk deployment
 return [
 
     /*

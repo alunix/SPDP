1f760828740cdcf1ac04f402241452fd2a03ca24
bezane97 <syazany1997@gmail.com>
2019-07-13 01:02:31 +0800

    Revert "Database for RDS"
    This reverts commit e7ec3ce48a31aeefb3f48cecdbe222ff4b6c1a16.

--- config/database.php
+++ config/database.php
@@ -47,11 +47,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => RDS_HOSTNAME,
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => RDS_DB_NAME,
-            'username' => RDS_USERNAME,
-            'password' => RDS_PASSWORD,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

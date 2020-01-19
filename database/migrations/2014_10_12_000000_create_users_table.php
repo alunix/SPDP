4577fda3d1b18b93a2a6b73b901e6471f1be71c7
syaz+1-at-981900451754 <syaz@biztory.com.my>
2020-01-19 08:45:18 +0800

    Working on laravel passport

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use SPDP\Fakulti;
+use Carbon\Carbon;
 
 class CreateUsersTable extends Migration
 {
@@ -19,17 +23,40 @@ class CreateUsersTable extends Migration
             $table->string('role');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('api_token', 80)
-            ->unique()
-            ->nullable()
-            ->default(null);
-            // $table->integer('fakulti_id')->unsigned()->nullable();
+            $table->integer('fakulti_id')->nullable()->unsigned();
+            
             $table->timestamp('email_verified_at')->nullable();
-
-            // $table->foreign('fakulti_id')->references('fakulti_id')->on('fakultis');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('fakulti_id')->references('fakulti_id')->on('fakultis');
         });
+
+        // $ftsm_id = Fakulti::where('kod', 'ftsm')->first()->value('fakulti_id');
+
+        DB::table('users')->insert(
+            [
+                [
+                    'name' => 'Dalbir Singh',
+                    'role' => 'pjk',
+                    'email' => 'dalbir@gmail.com',
+                    'password' => Hash::make('popo97'),
+                    'fakulti_id' => null,
+                    'email_verified_at' => null,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+                [
+                    'name' => 'Fakulti',
+                    'role' => 'fakulti',
+                    'email' => 'fakulti@gmail.com',
+                    'password' => Hash::make('popo97'),
+                    'fakulti_id' => 12,
+                    'email_verified_at' => null,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                    ]
+            ]
+        );
     }
 
     /**
